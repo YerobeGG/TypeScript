2c3de8f149e84c2fd0871042acfcbeca0b5c861b
YerobeGG <zlifeinvader@gmail.com>
2023-08-08 22:34:12 +0100

    Add

--- Iniciacion/firststeps.ts
+++ Iniciacion/firststeps.ts
@@ -1,2 +1,8 @@
-let numbers: number = 1
+let numbers: number = 10000
 console.log(numbers)
+
+let greetings: string = 'Good Morning Sir'
+console.log(greetings.toUpperCase());
+
+const copy: string = greetings.replace("Morning", "Night");
+console.log(greetings, copy)
