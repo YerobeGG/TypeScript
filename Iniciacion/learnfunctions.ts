33fda79df6d2ff43d58a2dd0f326fdae7bb1953e
YerobeGG <zlifeinvader@gmail.com>
2023-08-10 19:32:18 +0100

    Agregadas funciones

---
+++ Iniciacion/learnfunctions.ts
@@ -0,0 +1,18 @@
+function addDefinedNums(firstnum: number, secondnum: number){
+    return firstnum + secondnum
+}
+
+console.log(addDefinedNums(10, 20))
+
+
+function addNum2(num: number){
+    return num + 2
+}
+
+console.log(addNum2(10))
+
+function toUpeer(value: string){
+    return value.toUpperCase()
+}
+
+console.log(toUpeer('hola, buenas tardes'))
